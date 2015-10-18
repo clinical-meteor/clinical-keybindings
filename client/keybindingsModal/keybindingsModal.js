233e6287e33fd80ef97574f2f6eaaa9f4d181c66
Abigail Watson <awatson1978@gmail.com>
2015-10-17 23:07:50 -0700

    bump to 0.0.5; active-layout keybindings defaults moved to app

--- client/keybindingsModal/keybindingsModal.js
+++ client/keybindingsModal/keybindingsModal.js
@@ -3,6 +3,10 @@ Meteor.startup(function () {
   Session.setDefault('mainPanelIsCard', true);
   Session.setDefault('showSearchbar', true);
   Session.setDefault('showNavbars', true);
+  Session.setDefault('showSidebar', false);
+  // Session.setDefault('hasPageBorder', false);
+  // Session.setDefault('hasPagePadding', true);
+  // Session.setDefault('pageBackgroundIsWhite', false);
 
   Mousetrap.bind('ctrl+command+k', function () {
     Session.toggle('show_keybindings');
@@ -13,6 +17,9 @@ Meteor.startup(function () {
     Session.toggle('showOutboxCard');
     Session.toggle('showFormBuilderCard');
   });
+  Mousetrap.bind('ctrl+command+i', function () {
+    Session.toggle('showInboxCard');
+  });
   Mousetrap.bind('ctrl+command+r', function () {
     Session.toggle('showAccountCard');
     Session.toggle('showThemeCard');
@@ -21,13 +28,34 @@ Meteor.startup(function () {
   Mousetrap.bind('ctrl+command+n', function () {
     Session.toggle('showNavbars');
   });
+  Mousetrap.bind('ctrl+command+m', function () {
+    Session.toggle('showSidebar');
+  });
   Mousetrap.bind('ctrl+command+s', function () {
     Session.toggle('showSearchbar');
   });
   Mousetrap.bind('ctrl+command+c', function () {
     Session.toggle('mainPanelIsCard');
   });
-
+  Mousetrap.bind('ctrl+command+w', function () {
+    Session.toggle('wideCard');
+  });
+  Mousetrap.bind('ctrl+command+p', function () {
+    Session.toggle('pageNavbars');
+  });
+  Mousetrap.bind('ctrl+command+y', function () {
+    Session.toggle('showOutboxCard', true);
+    Session.toggle('outboxCardOpen', true);
+  });
+  Mousetrap.bind('ctrl+command+b', function () {
+    Session.toggle('hasPageBorder', true);
+  });
+  Mousetrap.bind('ctrl+command+p', function () {
+    Session.toggle('hasPagePadding', true);
+  });
+  Mousetrap.bind('ctrl+command+o', function () {
+    Session.toggle('pageBackgroundIsWhite');
+  });
 });
 
 
