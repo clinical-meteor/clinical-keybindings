4036722be96d682047baa102b03272f5fb397538
Abigail Watson <awatson1978@gmail.com>
2015-10-18 19:30:02 -0700

    bump dep on clinical:modals

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:keybindings',
-  version: '1.0.7',
+  version: '1.0.8',
   summary: 'Keybindings modal for  ClinicalFramework apps.',
   git: 'https://github.com/clinical-meteor/clinical-keybindings',
   documentation: 'README.md'
@@ -12,7 +12,7 @@ Package.onUse(function (api) {
   api.use('meteor-platform');
   api.use('grove:less@0.1.1');
   api.use('awatson1978:fonts-helveticas@1.0.4');
-  api.use('clinical:modals@0.0.3');
+  api.use('clinical:modals@1.0.5');
   api.use('mousetrap:mousetrap@1.4.6');
 
   api.addFiles('client/keybindingsModal/keybindingsModal.html', 'client');
