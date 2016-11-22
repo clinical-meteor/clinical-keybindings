e3d5ad8c62b6e9082455bec1e6e00b5279a9803b
Abigail Watson <awatson1978@gmail.com>
2016-11-21 19:21:32 -0600

    showVideoBackground

--- client/keybindings.js
+++ client/keybindings.js
@@ -1,4 +1,4 @@
-Meteor.startup(function () {
+Meteor.startup(function() {
   //console.log('Configuring keybindings...');
 
   Session.setDefault('show_keybindings', false);
@@ -19,98 +19,101 @@ Meteor.startup(function () {
 
   Session.setDefault('zoom', 100);
 
-  Mousetrap.bind('ctrl+command+t', function () {
+  Mousetrap.bind('ctrl+command+t', function() {
     Session.toggle('showThemingControls');
   });
-  Mousetrap.bind('ctrl+command+k', function () {
+  Mousetrap.bind('ctrl+command+k', function() {
     Session.toggle('show_keybindings');
     Session.toggle('showReactiveOverlay');
   });
-  Mousetrap.bind('ctrl+command+l', function () {
+  Mousetrap.bind('ctrl+command+l', function() {
     Session.toggle('showInboxCard');
     Session.toggle('showOutboxCard');
     Session.toggle('showFormBuilderCard');
   });
-  Mousetrap.bind('ctrl+command+i', function () {
+  Mousetrap.bind('ctrl+command+i', function() {
     Session.toggle('showInboxCard');
   });
-  Mousetrap.bind('ctrl+command+r', function () {
+  Mousetrap.bind('ctrl+command+r', function() {
     Session.toggle('showAccountCard');
     Session.toggle('showRightCard');
   });
-  Mousetrap.bind('ctrl+command+n', function () {
+  Mousetrap.bind('ctrl+command+n', function() {
     Session.toggle('showNavbars');
   });
   // Mousetrap.bind('ctrl+command+m', function () {
   //   Session.toggle('showSidebar');
   // });
-  Mousetrap.bind('ctrl+command+s', function () {
+  Mousetrap.bind('ctrl+command+s', function() {
     Session.toggle('showSearchbar');
   });
-  Mousetrap.bind('ctrl+command+a', function () {
+  Mousetrap.bind('ctrl+command+a', function() {
     console.log('ctrl+command+d');
     Session.toggle('useHierarchicalLayout');
   });
-  Mousetrap.bind('ctrl+command+c', function () {
+  Mousetrap.bind('ctrl+command+c', function() {
     Session.toggle('mainPanelIsCard');
   });
-  Mousetrap.bind('ctrl+command+w', function () {
+  Mousetrap.bind('ctrl+command+w', function() {
     Session.toggle('wideCard');
   });
 
-  Mousetrap.bind('ctrl+command+y', function () {
+  Mousetrap.bind('ctrl+command+y', function() {
     Session.toggle('showOutboxCard');
     Session.toggle('outboxCardOpen');
   });
-  Mousetrap.bind('ctrl+command+g', function () {
+  Mousetrap.bind('ctrl+command+g', function() {
     Session.toggle('hasPageBorder');
   });
-  Mousetrap.bind('ctrl+command+p', function () {
+  Mousetrap.bind('ctrl+command+p', function() {
     Session.toggle('hasPagePadding');
   });
 
 
-  Mousetrap.bind('ctrl+command+q', function () {
+  Mousetrap.bind('ctrl+command+q', function() {
     Session.toggle('navIsFullscreen');
   });
   // Mousetrap.bind('ctrl+command+h', function () {
   //   Session.toggle('pageLeftToWestRule');
   // });
-  Mousetrap.bind('ctrl+command+j', function () {
+  Mousetrap.bind('ctrl+command+j', function() {
     Session.toggle('pageIsWide');
   });
-  Mousetrap.bind('ctrl+command+r', function () {
+  Mousetrap.bind('ctrl+command+r', function() {
     Session.toggle('useCardLayout');
   });
-  Mousetrap.bind('ctrl+command+m', function () {
+  Mousetrap.bind('ctrl+command+m', function() {
     Session.toggle('hasPageVerticalPadding');
   });
-  Mousetrap.bind('ctrl+command+v', function () {
+  Mousetrap.bind('ctrl+command+f', function() {
     Session.toggle('useVerticalFences');
   });
-  Mousetrap.bind('ctrl+command+h', function () {
+  Mousetrap.bind('ctrl+command+h', function() {
     Session.toggle('useHorizontalFences');
   });
 
-  Mousetrap.bind('ctrl+command+b', function () {
+  Mousetrap.bind('ctrl+command+b', function() {
     Session.toggle('appSurfaceOffset');
   });
-  Mousetrap.bind('ctrl+command+e', function () {
+  Mousetrap.bind('ctrl+command+e', function() {
     Session.toggle('useEastFence');
   });
 
-  Mousetrap.bind('ctrl+command+z', function () {
+  Mousetrap.bind('ctrl+command+z', function() {
     Session.toggle('hasFooterPadding');
   });
-  Mousetrap.bind('ctrl+command+x', function () {
+  Mousetrap.bind('ctrl+command+x', function() {
     Session.toggle('wideSecondPanel');
   });
+  Mousetrap.bind('ctrl+command+v', function() {
+    Session.toggle('showVideoBackground');
+  });
 
-  Mousetrap.bind('ctrl+command+=', function () {
+  Mousetrap.bind('ctrl+command+=', function() {
     Session.set('zoom', Session.get('zoom') + 10);
     console.log('zoom', Session.get('zoom') + "%");
   });
-  Mousetrap.bind('ctrl+command+-', function () {
+  Mousetrap.bind('ctrl+command+-', function() {
     Session.set('zoom', Session.get('zoom') - 10);
     console.log('zoom', Session.get('zoom') + "%");
   });
--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'clinical:keybindings',
-  version: '1.3.3',
+  version: '1.3.4',
   summary: 'Keybindings modal for  ClinicalFramework apps.',
   git: 'https://github.com/clinical-meteor/keybindings',
   documentation: 'README.md'
 });
 
-Package.onUse(function (api) {
+Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
 
   api.use('meteor-platform');
@@ -15,16 +15,19 @@ Package.onUse(function (api) {
   api.use('clinical:fonts@1.1.0');
   api.use('mousetrap:mousetrap@1.4.6');
 
-  api.addFiles('client/components/keybindingsModal/keybindingsModal.html', 'client');
-  api.addFiles('client/components/keybindingsModal/keybindingsModal.js', 'client');
-  api.addFiles('client/components/keybindingsModal/keybindingsModal.less', 'client');
+  api.addFiles('client/components/keybindingsModal/keybindingsModal.html',
+    'client');
+  api.addFiles('client/components/keybindingsModal/keybindingsModal.js',
+    'client');
+  api.addFiles('client/components/keybindingsModal/keybindingsModal.less',
+    'client');
 
   api.addFiles('client/keybindings.js', 'client');
 
   api.export('keybindingsModal');
 });
 
-Package.onTest(function (api) {
+Package.onTest(function(api) {
   api.use('tinytest');
   api.use('clinical:keybindings');
 });
